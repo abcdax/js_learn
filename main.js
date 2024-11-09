d7630037d7195f45748e60ec7c359bc7451a4ae9
douaa <dbelhachmi33@gamil.com>
2024-11-09 17:54:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('h1').style.color= 'red';//selection d'ecritur en h1 et ecrit en red 
+document.write('hello');//erit sur interface
+
+console.log('hello');//erit sur console  
+
+window.alert('hello world ');//fenetre d'alerte
